e9858249ba69adaea11c95991735af87c7cb71d3
Mjoel4708 <mjoel4708@gmail.com>
2020-11-19 22:52:52 +0300

    Creating Front end..

--- src/App.js
+++ src/App.js
@@ -1,57 +1,58 @@
 import React, { Component } from "react";
+import contract from "truffle-contract"
 import ProcurementContract from "./contracts/Procurement.json";
-import getWeb3 from "./getWeb3";
+
+
 
 import "./App.css";
+import getWeb3 from "./getWeb3";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = ProcurementContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        ProcurementContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+  state = { storageValue: [] }
+  async componentDidMount() {
+    const web3 = await getWeb3()
+    const provider = () => {
+      // If the user has MetaMask:
+      if (typeof web3 !== 'undefined') {
+        return web3.currentProvider
+      } else {
+        console.error("You need to install MetaMask for this app to work!")
+      }
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    //await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    //const response = await contract.methods.get().call();
-
-    // Update state with the result
-    //this.setState({ storageValue: response });
-  };
+    try {
+      
+      const accounts = await web3.eth.getAccounts() // Get user's ETH addresses
+      //console.log(addresses)
+      
+      
+      
+      const getInstance = async (web3, ProcurementContract) => {
+        // get network ID and the deployed address
+        const networkId = await web3.eth.net.getId()
+        const deployedAddress = ProcurementContract.networks[networkId].address
+      
+        // create the instance
+        const instance = new web3.eth.Contract(ProcurementContract.abi, deployedAddress)
+        return instance
+      }
+      //get user
+      const storage = await getInstance(web3, ProcurementContract)
+      const  id  = await storage.methods.getTender('1');
+      
+      console.log(id)
+      this.setState({ storageValue: id })
+    } catch (err) {
+      console.error(err)
+    }
+    
+  }
+  
+  
+  
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+    
+    
     return (
       <div className="App">
         <h1>Good to Go!</h1>
@@ -64,7 +65,9 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
-        <div>The stored value is: </div>
+        <div>The stored value is: 
+          
+        </div>
       </div>
     );
   }
